b2b2640ffbfd4ba629c8cc8f830c3caad7772df4
Usman Ali <usmanali9525@gmail.com>
2023-07-27 12:09:18 +0500

    New

--- barcode_reader.py
+++ barcode_reader.py
@@ -5,14 +5,14 @@ def load_image(image_path):
     return cv2.imread(image_path)
 
 # Provide the image file path
-image_path = "Images/barcodereader.png"
+image_path = "Images/captured_image.png"
 image = load_image(image_path)
 
 def decode_barcode(image):
     
     #Preprocessing Images
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert the image to grayscale for faster processing
-    blurred_image = cv2.GaussianBlur(gray_image, (9, 9), 0) # Apply Gaussian blur to reduce noise (adjust the kernel size as needed)
+    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0) # Apply Gaussian blur to reduce noise (adjust the kernel size as needed)
     _, threshold_image = cv2.threshold(blurred_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) # Apply Otsu thresholding to binarize the image
     barcodes = decode(threshold_image) # Find barcodes in the thresholded image
     barcodes = decode(gray_image) # Find barcodes in the image
--- barcode_reader_external.py
+++ barcode_reader_external.py
@@ -2,9 +2,12 @@ import cv2
 from pyzbar.pyzbar import decode
 
 def decode_barcode(image):
-    # Convert the image to grayscale for faster processing
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert the image to grayscale for faster processing
+    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0) # Apply Gaussian blur to reduce noise (adjust the kernel size as needed)
+    _, threshold_image = cv2.threshold(blurred_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) # Apply Otsu thresholding to binarize the image
+    barcodes = decode(threshold_image) # Find barcodes in the thresholded image
+    barcodes = decode(gray_image) # Find barcodes in the image
     # Find barcodes in the image
     barcodes = decode(gray_image)
     
@@ -13,9 +16,11 @@ def decode_barcode(image):
         barcode = barcodes[0]
         barcode_data = barcode.data.decode('utf-8')
         barcode_type = barcode.type
-        return barcode_data, barcode_type
+        x, y, w, h = barcode.rect
+        return barcode_data, barcode_type, (x, y, w, h)
+    
     else:
-        return None, None
+        return None, None, None
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -24,16 +29,19 @@ def main():
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
 
-        barcode_data, barcode_type = decode_barcode(frame)
+        barcode_data, barcode_type, barcode_box = decode_barcode(frame)
         if barcode_data:
             print(f"Barcode Type: {barcode_type}")
             print(f"Barcode Data: {barcode_data}")
+
+            x, y, w, h = barcode_box
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             # Save the captured frame with the barcode as an image
             cv2.imwrite("captured_image.png", frame)
             
             # Uncomment the following line if you want to stop the loop after detecting a barcode
-            # break
+            break
         
         cv2.imshow("Camera", frame)
 
